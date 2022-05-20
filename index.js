874ef6eddf9bd087237e8cf073542f09ebe42c74
Md Sarmad <28mdsarmad99@gmail.com>
2022-05-20 20:29:33 +0530

    compiler code

---
+++ index.js
@@ -0,0 +1,31 @@
+// var axios = require('axios');
+function postData() {
+    var data = JSON.stringify({
+        "code": `public class program{
+                    public static void main(String [] args){
+                        System.out.println(5+5+6);
+                      }
+                    }`,
+        "language": "java",
+        "input": ""
+    });
+
+    var config = {
+        method: 'post',
+        url: 'https://codexweb.netlify.app/.netlify/functions/enforceCode.',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: data
+    };
+
+    axios(config)
+        .then(function (response) {
+            console.log(response.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+postData();
